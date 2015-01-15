93974b233381ac7268df609bd46fa13c6b0db383
pv <pat@cs.umass.edu>
2015-01-14 20:00:10 -0500

    twitter stream take sentiment arg, fix joblib import

--- pi/src/python/twitter_functions.py
+++ pi/src/python/twitter_functions.py
@@ -19,47 +19,44 @@ model, char_vectorizer, word_vectorizer, lexicons = load_serial()
 
 # post a tweet to @plant_jones
 def sendTweet(tweetStr):
-   api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
-   api.update_status(status=tweetStr)
-   print "Tweeted: " + tweetStr
+    api = Twython(apiKey, apiSecret, accessToken, accessTokenSecret)
+    api.update_status(status=tweetStr)
+    print "Tweeted: " + tweetStr
+
 
 # class to handle streaming random tweets
 class MyStreamer(TwythonStreamer):
-    
-    #    def __init__(self, **kwargs):
-    #    txt2vec = joblib.load('saved_model/vectorizer.pickle')
-    #    model = joblib.load('saved_model/svm_model.pk1')
+
+    target_sentiment = "negative"
+
+    def set_sentiment(self, target_sentiment):
+        MyStreamer.target_sentiment = "\""+target_sentiment+"\""
 
     def on_success(self, data):
         if 'text' in data:
             ascii_tweet = data['text']
             utf_tweet = ascii_tweet.encode('utf-8')
             vectors = create_vectors([utf_tweet], word_vectorizer, char_vectorizer, lexicons)
-            sentiment = model.predict(vectors)[0]
+            tweet_sentiment = model.predict(vectors)[0]
             # estimate the sentiment of the tweet
 
-            print (utf_tweet + '\t' + sentiment + '\n')
-            # Want to disconnect after the first result?
-            # self.disconnect()
+            if tweet_sentiment == self.target_sentiment:
+                print (utf_tweet + '\t' + tweet_sentiment + '\n')
+                # sendTweet(utf_tweet)
+                # Want to disconnect after the first result?
+                # self.disconnect()
 
     def on_error(self, status_code, data):
         print status_code, data
-       
+
 
 # grab a random tweet
-def randomTweetFromStream():
-    stream = MyStreamer(apiKey,apiSecret,accessToken,accessTokenSecret)
+def randomTweetFromStream(target_sentiment):
+    stream = MyStreamer(apiKey, apiSecret, accessToken, accessTokenSecret)
+    stream.set_sentiment(target_sentiment)
     stream.statuses.filter(track='water', language='en')
 
 
-
 if __name__ == "__main__":
-
-#    if len(sys.argv) < 2:
-#        print ("Must supply tweet string")
-#        sys.exit()
-#
-#    tweetStr = sys.argv[1]
-#    sendTweet(tweetStr)
-
-    randomTweetFromStream()
+    sentiment = "negative" if sys.argv[1] == 0 else "positive"
+    randomTweetFromStream(sentiment)
