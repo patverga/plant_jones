8b42a66f7c2032405dca9174275fc862d9542874
pv <pat@cs.umass.edu>
2015-01-15 23:04:31 -0500

    fix server client

--- pi/src/twitter_functions_server.py
+++ pi/src/twitter_functions_server.py
@@ -5,7 +5,7 @@ import socket
 from twython import Twython
 from twython import TwythonStreamer
 
-#sys.path.insert(0, '/home/pemma/plant_jones/pi/src/python/sentiment_analysis')
+# sys.path.insert(0, '/home/pemma/plant_jones/pi/src/python/sentiment_analysis')
 from sentiment_analysis.sentiment_analysis_model import load_serial, create_vectors
 
 # your twitter consumer and access information
@@ -18,6 +18,51 @@ saved_model_dir = 'saved_model/'
 # sentiment analysis model
 model, char_vectorizer, word_vectorizer, lexicons = load_serial()
 
+# post a tweet to @plant_jones
+def sendTweet(tweetStr):
+    api = Twython(apiKey, apiSecret, accessToken, accessTokenSecret)
+    api.update_status(status=tweetStr)
+    print "Tweeted: " + tweetStr
+
+
+# class to handle streaming random tweets
+class MyStreamer(TwythonStreamer):
+    target_sentiment = '\"negative\"'
+
+    def initialize(self, target_sentiment):
+        print("Looking for tweets with " + target_sentiment + " sentiment")
+        MyStreamer.target_sentiment = "\""+target_sentiment+"\""
+
+    def on_success(self, data):
+        try:
+            if 'text' in data:
+                ascii_tweet = data['text']
+                utf_tweet = ascii_tweet.encode('utf-8')
+                vectors = create_vectors(
+                    [utf_tweet], word_vectorizer, char_vectorizer, lexicons)
+                tweet_sentiment = str(model.predict(vectors)[0])
+                # estimate the sentiment of the tweet
+
+                if tweet_sentiment == self.target_sentiment:
+                    print (utf_tweet + '\t' + tweet_sentiment + '\n')
+                    sendTweet(utf_tweet)
+                    # Want to disconnect after the first result?
+                    self.disconnect()
+        except:
+            pass
+
+    def on_error(self, status_code, data):
+        print status_code, data
+
+
+# grab a random tweet
+def randomTweetFromStream(sentiment_code):
+    target_sentiment = 'positive' if sentiment_code == "1" else 'negative'
+    stream = MyStreamer(apiKey, apiSecret, accessToken, accessTokenSecret)
+    stream.initialize(target_sentiment)
+    stream.statuses.filter(track='water', language='en')
+
+
 # set up server
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 10000)
@@ -28,59 +73,17 @@ sock.bind(server_address)
 sock.listen(1)
 while True:
     # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
+    print >> sys.stderr, 'waiting for a connection'
     connection, client_address = sock.accept()
     try:
-        print >>sys.stderr, 'connection from', client_address
+        print >> sys.stderr, 'connection from', client_address
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
             if not data:
                 break
+            print >> sys.stderr, 'received "%s"' % data
+            randomTweetFromStream(data)
     finally:
         # Clean up the connection
         connection.close()
-
-# post a tweet to @plant_jones
-def sendTweet(tweetStr):
-    api = Twython(apiKey, apiSecret, accessToken, accessTokenSecret)
-    api.update_status(status=tweetStr)
-    print "Tweeted: " + tweetStr
-
-
-# class to handle streaming random tweets
-class MyStreamer(TwythonStreamer):
-
-    target_sentiment = "negative"
-
-    def initialize(self, target_sentiment):
-        MyStreamer.target_sentiment = "\""+target_sentiment+"\""
-
-    def on_success(self, data):
-        try:
-            if 'text' in data:
-                ascii_tweet = data['text']
-                utf_tweet = ascii_tweet.encode('utf-8')
-                vectors = create_vectors(
-                    [utf_tweet], word_vectorizer, char_vectorizer, lexicons)
-                tweet_sentiment = model.predict(vectors)[0]
-                # estimate the sentiment of the tweet
-
-                if tweet_sentiment == self.target_sentiment:
-                    print (utf_tweet + '\t' + tweet_sentiment + '\n')
-                    # sendTweet(utf_tweet)
-                    # Want to disconnect after the first result?
-                    # self.disconnect()
-        except:
-            pass
-
-    def on_error(self, status_code, data):
-        print status_code, data
-
-
-# grab a random tweet
-def randomTweetFromStream(target_sentiment):
-    stream = MyStreamer(apiKey, apiSecret, accessToken, accessTokenSecret)
-    stream.initialize(target_sentiment)
-    stream.statuses.filter(track='water', language='en')
